87d81ccc06ed667f1605aaed9c6844b3989ac80e
Andrew <dzombak88@gmail.com>
2019-05-27 05:55:57 +0300

    Delete method and forEach loop for Statuses

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -2,5 +2,17 @@ class Config {
     final static String BASE_URI = "https://petstore.swagger.io/v2";
 
     final static String GET_PET_BY_ID = "pet/{petID}";
+    final static String DELETE_PET_BY_ID = "pet/{petID}";
     final static String GET_PET_BY_STATUS = "pet/findByStatus";
+
+    protected enum Statuses{
+        AVAILABLE,
+        PENDING,
+        SOLD
+    }
+
+    static String petID = "12032705";
+
+    static int successfullStatusCode = 200;
+
 }
--- src/test/java/PetStoreTest.java
+++ src/test/java/PetStoreTest.java
@@ -8,18 +8,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
+
 public class PetStoreTest {
-
-
-
-    private enum status{
-        AVAILABLE,
-        PENDING,
-        SOLD
-    }
-
-
-        //static RestAssured.baseURI = Config.BASE_URI;
 
     static  {
         RestAssured.baseURI = Config.BASE_URI;
@@ -28,42 +18,56 @@ public class PetStoreTest {
     @Test
     public void getPetbyIdTest() {
 
-        long petId = 2019052412031988L;
+            RestAssured.given()
+                    .pathParam("petID", Config.petID)
+                    .get(Config.GET_PET_BY_ID)
+                    .then()
+                    .log().all()
+                    .statusCode(Config.successfullStatusCode);
+        }
 
-        RestAssured.given()
-                //.param("""PetStoreTest.petId")
-                //.basePath("")
-                //.log().uri()
-                .get(Config.GET_PET_BY_ID)
-                .then()
-                .log().all()
-                .statusCode(200);
-    }
+    @Test
+    public void deletePetbyId () {
+
+           RestAssured.given()
+                   .pathParam("petID", Config.petID)
+                   .delete(Config.DELETE_PET_BY_ID)
+                   .then()
+                   .log().all();
+                   //.header()
+        //System.out.println(response.statusCode());
+
+
+        }
+
+
 
     @Test
     public void getPetbyStatus() {
 
-        RestAssured.given()
-                .param("status", status.AVAILABLE)
-                //.param()
-                //.basePath()
-                //.log().uri()
-                .get(Config.GET_PET_BY_STATUS)
-                .then()
-                .log().all()
-                .statusCode(200);
+        for (Config.Statuses status : Config.Statuses.values()) {
 
-        Response response = RestAssured.given().get(Config.GET_PET_BY_STATUS).thenReturn();
-        ResponseBody body = response.getBody();
-        System.out.println(body);
-        System.out.println(body.prettyPrint());
-        //System.out.println(response.prettyPrint());
-
-        //JSONObject data = new JSONObject(response);
-        //System.out.println(data);
-
-        //JSONArray ja = new JSONArray(response.print());
-        //System.out.println(ja);
-
+            RestAssured.given()
+                    .param("status", status)
+                    .log().uri()
+                    .get(Config.GET_PET_BY_STATUS)
+                    .then()
+                    .log().all()
+                    .statusCode(Config.successfullStatusCode);
+        }
     }
-}
+    }
+
+
+
+//    Response response = RestAssured.given().get(Config.GET_PET_BY_STATUS).thenReturn();
+//    ResponseBody body = response.getBody();
+//        System.out.println(body);
+//                System.out.println(body.prettyPrint());
+//System.out.println(response.prettyPrint());
+//
+//JSONObject data = new JSONObject(response);
+//System.out.println(data);
+//
+//JSONArray ja = new JSONArray(response.print());
+//System.out.println(ja);
